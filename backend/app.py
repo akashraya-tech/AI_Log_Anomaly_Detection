11455b9b27ead7e4417a550f871f4a54f1deccc3
akashraya-tech <akashraya060@gmail.com>
2026-08-23 11:49:02 +0530

    Added session timeout

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,11 @@ app = Flask(
     __name__,
     template_folder="../templates"
 )
+
 app.secret_key = "akash_project_secret"
+
+app.permanent_session_lifetime = timedelta(minutes=1)
+
 CORS(app)
 
 # ========================================
@@ -419,7 +423,8 @@ def login():
         password = request.form["password"]
 
         if username == "admin" and password == "AILog@2026#Secure":
-
+         
+            session.permanent = True
             session["logged_in"] = True
 
             return redirect("/")
